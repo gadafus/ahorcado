2867575e0b147d7e961767d3eaf5a95061cfa5fa
Your Name <you@example.com>
2020-07-10 14:02:32 +0200

    corregido vidas

--- src/components/Vidas.vue
+++ src/components/Vidas.vue
@@ -1,33 +1,32 @@
 <template>
-  <div v-bind:class="restaVida()">{{vidas}}</div>
+  <div >{{vidas}}</div>
 </template>
 
 <script>
 import bus from "../bus";
 
 export default {
-    name: "Vidas",
-    props: {
-        
-    },
+  name: "Vidas",
+  props: {},
 
   data() {
     return {
       vidas: 5
     };
   },
+  /* ,
 
   methods: {
     restaVidas: function() {
       bus.$on('falloLetra', this.vidas = this.vidas -1);
       return this.vidas;
     }
-  } 
-  /* created() {
-    bus.$on("falloLetra", restaVida => {
+  }  */
+  created() {
+    bus.$on("falloLetra", () => {
       this.vidas = this.vidas - 1;
     });
-  } */
+  }
 };
 </script>
 
